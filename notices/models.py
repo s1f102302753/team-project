5d9e511f7b8d95ba66c613bbec528c0605f609a8
1F10230275 <s1f102302753@iniad.org>
2025-10-16 23:09:08 +0900

    apiのデータベース設計とtemplatesの統一、API取得処理の追加

--- notices/models.py
+++ notices/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
 from django.db import models
 from users.models import Municipality, CustomUser
+from django.contrib.auth.models import User
 
-
-
+# Noticeモデル 行政・自治体からのお知らせ
 class Notice(models.Model):
     """行政・自治体からのお知らせ（回覧板の記事など）"""
     title = models.CharField(max_length=200)
@@ -22,3 +23,13 @@ class Notice(models.Model):
 
     def __str__(self):
         return f"[{self.municipality}] {self.title}"
+
+# Postモデル 掲示板の記事
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
