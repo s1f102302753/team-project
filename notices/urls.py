a182f63136080656b5b5b5565bb72e405e885bd8
1F10230275 <s1f102302753@iniad.org>
2025-10-24 19:40:17 +0900

    掲示板の新規投稿機能ととリスト表示化

--- notices/urls.py
+++ notices/urls.py
@@ -10,8 +10,5 @@ urlpatterns = [
     path('posts/', views.post_list, name='post_list'),
     path('posts/new/', views.post_create, name='post_create'),
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),
-
     path('api/notices/', views.api_notices, name='api_notices'),
-    path('api/posts/', views.api_posts, name='api_posts'),
-
 ]
